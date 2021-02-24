bbe25ee858d4291a3feeecef9144b801afe29167
Hansjason97 <ultiokiller@gmail.com>
2021-02-24 17:28:53 +0100

    fxing colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { BreakpointObserver } from '@angular/cdk/layout';
 import {TranslateService} from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-root',
@@ -9,15 +9,14 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'MyPossa';
-  Laptop = true;
-  constructor (private breakpointObserver: BreakpointObserver,
-              public translate: TranslateService){
+
+  constructor (public translate: TranslateService){
                 /**Translation service */
       translate.addLangs(['EN', 'FR']);
       translate.setDefaultLang('EN');
       const browserLang = translate.getBrowserLang();
       translate.use(browserLang.match(/EN|FR/) ? browserLang : 'EN');
-
+      
   }
 
    /**Translation functions */
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,9 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PartnersComponent } from './partners/partners.component';
@@ -16,17 +14,13 @@ import { OurServicesComponent } from './our-services/our-services.component';
 import { OurMapComponent } from './our-map/our-map.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component'; 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatListModule} from '@angular/material/list';
-import {MatSelectModule} from '@angular/material/select';
 
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import {HttpClientModule, HttpClient} from '@angular/common/http';
 import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -48,16 +42,10 @@ export function HttpLoaderFactory(http: HttpClient) {
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
     NgbModule,
-    MatMenuModule,
-    MatSidenavModule,
-    MatExpansionModule,
-    MatListModule,
-    MatSelectModule,
+    MaterialModule,
     CarouselModule.forRoot(),
+    FlexLayoutModule,
     HttpClientModule,
     HttpClientModule,
     TranslateModule.forRoot({
