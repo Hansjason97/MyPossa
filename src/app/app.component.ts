76d59337dc812721e71a174ea02831b93b8771e6
Hansjason97 <ultiokiller@gmail.com>
2021-03-11 17:32:47 +0100

    making the goTop button global on the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 
 @Component({
@@ -8,7 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyPossa';
+  isShow: boolean;
+  topPosToStartShowing = 100;
 
   constructor (){ }
    
+  @HostListener('window:scroll')
+  checkScroll() {
+      
+    // window의 scroll top
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  // TODO: Cross browsing
+  gotoTop() {
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import { Title } from '@angular/platform-browser';
 import { mergeMap } from 'rxjs/operators';
@@ -9,8 +9,7 @@ import { mergeMap } from 'rxjs/operators';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  isShow: boolean;
-  topPosToStartShowing = 100;
+
 
   constructor(public translate: TranslateService, private titleService: Title) { 
     translate.onLangChange.pipe(
@@ -19,29 +18,6 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-  }
-  @HostListener('window:scroll')
-  checkScroll() {
-      
-    // window의 scroll top
-    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
-
-    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    
-    if (scrollPosition >= this.topPosToStartShowing) {
-      this.isShow = true;
-    } else {
-      this.isShow = false;
-    }
-  }
-
-  // TODO: Cross browsing
-  gotoTop() {
-    window.scroll({ 
-      top: 0, 
-      left: 0, 
-      behavior: 'smooth' 
-    });
   }
 }
 
